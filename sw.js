d29fa9d2ccea90c2a1c6d7f6dcfbcd64577505d8
siranildo518-coder <siranildo518@gmail.com>
2026-09-12 23:01:13 -0300

    Atualiza cache para correção de cores nativas

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // THOR LOTERIAS - Service Worker
-// Sincronizado com tema dinâmico por loteria.
-const CACHE_NAME = 'thor-loterias-2026-09-12-tema-megasena-verde-04';
+// Cores dinâmicas usando a cor nativa de cada loteria.
+const CACHE_NAME = 'thor-loterias-2026-09-12-cor-nativa-loteria-05';
 const CORE = ['./','./index.html','./app-main.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install',(event)=>{event.waitUntil(caches.open(CACHE_NAME).then((cache)=>Promise.all(CORE.map((url)=>fetch(url,{cache:'no-store'}).then((res)=>res&&res.ok?cache.put(url,res.clone()):null).catch(()=>null)))));self.skipWaiting();});
 self.addEventListener('message',(event)=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
