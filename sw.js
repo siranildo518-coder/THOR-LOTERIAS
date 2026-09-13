dea8feacee089ee5844b5fcaff704cbe0aee5e67
siranildo518-coder <siranildo518@gmail.com>
2026-09-12 22:39:50 -0300

    Sincroniza cache do fechamento compacto

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // THOR LOTERIAS - Service Worker
-// Versão sincronizada com index.html: título e cores acompanham a loteria escolhida.
-const CACHE_NAME = 'thor-loterias-2026-09-12-home-loteria-dinamica-02';
+// Versão sincronizada com index.html: fechamento personalizado compacto.
+const CACHE_NAME = 'thor-loterias-2026-09-12-fechamento-compacto-01';
 const CORE = ['./','./index.html','./app-main.html','./manifest.json','./icon-192.png','./icon-512.png','./icon-512-maskable.png'];
 self.addEventListener('install',(event)=>{event.waitUntil(caches.open(CACHE_NAME).then((cache)=>Promise.all(CORE.map((url)=>fetch(url,{cache:'no-store'}).then((res)=>res&&res.ok?cache.put(url,res.clone()):null).catch(()=>null)))));self.skipWaiting();});
 self.addEventListener('message',(event)=>{if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();});
