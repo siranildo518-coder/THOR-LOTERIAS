0c9fc4063ecb04bfa4189c60961b59e049a425e2
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 19:26:56 -0300

    THOR 7 V2.3: sincronizar remoção dos botões do menu

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V2.2 - aba antiga Começo/Final substituída por nova tela branca.
-importScripts('./sw-core-thor7.js?v=thor7-v2-2');
+// THOR 7 V2.3 - removidos Análise e Começo Final do menu lateral.
+importScripts('./sw-core-thor7.js?v=thor7-v2-3');
