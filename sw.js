4846ad888bd05ee30c7f19a431a3f74860ed3a55
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 18:57:53 -0300

    THOR 7 v2: sincronizar nova tela branca Começo e Final

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V2.1 - Começo e Final em branco.
-importScripts('./sw-core-thor7.js?v=thor7-v2-1');
+// THOR 7 V2.2 - aba antiga Começo/Final substituída por nova tela branca.
+importScripts('./sw-core-thor7.js?v=thor7-v2-2');
