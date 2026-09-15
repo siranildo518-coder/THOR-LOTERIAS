508f8c1d278151d365116b758c5ed34872e62d09
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 20:20:47 -0300

    THOR 7 V2.7: adicionar telas novas em branco

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V2.6 - Análise e Começo Final removidos; arquivos e cache sincronizados.
-importScripts('./sw-core-thor7.js?v=thor7-v2-6');
+// THOR 7 V2.7 - novos botões abrem telas independentes em branco; arquivos e cache sincronizados.
+importScripts('./sw-core-thor7.js?v=thor7-v2-7');
