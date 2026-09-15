d98c2af3bedf1ab4c81d2a7e058439c3f9223c6c
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 06:46:53 -0300

    THOR 8: atualizar cache da conferencia automatica

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR 8 - mantém o service worker completo da Base 142 e força a atualização conjunta do index e conferência.
-importScripts('./sw-core-thor7.js?v=thor8-confere-1');
+importScripts('./sw-core-thor7.js?v=thor8-confere-2');
