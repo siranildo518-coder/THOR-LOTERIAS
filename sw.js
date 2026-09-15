752b02a2de313a61b2ec1332b9b7d9bcf4eb39d0
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 06:34:42 -0300

    THOR 7: atualizar SW junto com voltar físico

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR 7 - mantém o service worker completo da Base 142 e força a atualização conjunta do index.
-importScripts('./sw-core-thor7.js?v=thor7-back-1');
+importScripts('./sw-core-thor7.js?v=thor7-back-2');
