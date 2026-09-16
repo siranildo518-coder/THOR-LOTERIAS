9f50d64b4bacdab26361772a06e0fe8bd192c77a
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 06:53:12 -0300

    THOR 7 V3.30 adicionar topo personalizado para todas as loterias

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.29 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-29');
+// THOR 7 V3.30 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-30');
