623b20613ba46c773ecc4a01e8bac987543398a1
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 17:24:32 -0300

    THOR 7 V3.37 fixar topos das loterias

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.36 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-36');
+// THOR 7 V3.37 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-37');
