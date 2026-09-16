2cf0a928dd832a5ab0ea45751331412af1ce3d0f
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 18:21:30 -0300

    THOR 7 V3.41 melhorar contraste da análise

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.40 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-40');
+// THOR 7 V3.41 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-41');
