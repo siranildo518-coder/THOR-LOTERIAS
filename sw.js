f8db004e782a022464626b95aa3881b4a8e79414
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 05:44:10 -0300

    THOR 7 V3.22 padronizar tamanho dos botões do Gerador

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.21 - cabeçalho preenche todo o topo e botões menores; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-21');
+// THOR 7 V3.22 - todos os botões do Gerador com o mesmo tamanho; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-22');
