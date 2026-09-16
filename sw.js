a73315d0f96fd4de38a0b81fa532b25cb5f1ca73
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 18:15:54 -0300

    THOR 7 V3.40 adicionar símbolos na análise

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.39 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-39');
+// THOR 7 V3.40 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-40');
