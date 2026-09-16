4b126d3372ba13b7fceee1adbb1115abb8fb05f4
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 05:16:12 -0300

    THOR 7 V3.15 novo topo e cache sincronizado

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.14 - Análise > Gerar com quantidade digitável de 1 a 30 dezenas; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-14');
+// THOR 7 V3.15 - novo topo da tela inicial sem botão de menu; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-15');
