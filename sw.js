4bb4f26bab40e33e25e105a27c7fd06fb5e015dd
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 20:45:19 -0300

    THOR 7 V3.51 animar globo com bolinhas e coleta

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.50 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-50');
+// THOR 7 V3.51 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-51');
