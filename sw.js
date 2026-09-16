24ed1894dc240aed1f51c1ea37e4a613b2cce770
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 06:43:09 -0300

    THOR 7 V3.29 aplicar topo e cores dos resultados da Mega-Sena

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.28 - novo visual da tela interna do Fechamento Personalizado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-28');
+// THOR 7 V3.29 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-29');
