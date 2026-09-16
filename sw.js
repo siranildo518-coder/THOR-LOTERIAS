fc26fc1b98767674e643977d578f6c34e777be7a
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 23:33:53 -0300

    Sincroniza service worker com THOR 7 V3.7

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.6 - Resultado Oficial da Lotofácil 3780; carregamento e cache sincronizados.
-importScripts('./sw-core-thor7.js?v=thor7-v3-6');
+// THOR 7 V3.7 - frequência corrigida para analisar 100 ou mais concursos; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-7');
