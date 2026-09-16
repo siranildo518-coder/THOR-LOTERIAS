bbde59ba5334eeb70d45c92335d0088fea073856
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 19:28:12 -0300

    THOR 7 V3.44 trocar fontes roxas por brancas

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.43 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-43');
+// THOR 7 V3.44 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-44');
