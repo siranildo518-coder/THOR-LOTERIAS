37dfc89efc88a7a6db35a6c5c42e043e7319001e
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 20:04:41 -0300

    THOR 7 V3.47 compactar tabela de frequência

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.46 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-46');
+// THOR 7 V3.47 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-47');
