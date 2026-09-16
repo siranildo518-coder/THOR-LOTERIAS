427aaa4a3fbfd323a674b8712d1747b17c57f5cc
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 20:28:56 -0300

    THOR 7 V3.49 corrigir globo do conferidor

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.48 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-48');
+// THOR 7 V3.49 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-49');
