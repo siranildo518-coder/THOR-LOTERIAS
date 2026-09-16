a8f25989d289201a36108be7055d1acb6ce7506a
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 05:13:12 -0300

    V3.14 sincronizar cache da aba Gerar

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.13 - remove seta e botão Salvar Jogos do cabeçalho do Gerador de Palpites; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-13');
+// THOR 7 V3.14 - Análise > Gerar com quantidade digitável de 1 a 30 dezenas; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-14');
