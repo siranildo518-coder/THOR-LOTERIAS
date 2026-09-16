0697bcf26fc1abf7efdfa608e4daac0a15adca29
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 06:10:51 -0300

    THOR 7 V3.27 aplicar novo visual ao Fechamento Personalizado

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.26 - dezenas de 1 a 30 e base de análise de 3 a 100; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-26');
+// THOR 7 V3.27 - novo visual funcional do Fechamento Personalizado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-27');
