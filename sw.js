68918a509c5359a9fc900798e756406454d9da63
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 05:41:13 -0300

    THOR 7 V3.21 preencher todo o topo do Gerador

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.20 - botões das loterias menores no Gerador; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-20');
+// THOR 7 V3.21 - cabeçalho preenche todo o topo e botões menores; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-21');
