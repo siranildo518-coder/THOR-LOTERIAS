203f5a47c82bdf57182f2b7a807b1601e339d2fe
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 00:06:50 -0300

    THOR 7 V3.12 sincronizar cache

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.11 - atualização visual da Lotofácil corrigida; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-11');
+// THOR 7 V3.12 - restaura Análise e Combinações nos cards da tela inicial; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-12');
