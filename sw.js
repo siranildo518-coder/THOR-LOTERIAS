433704d4ca1d6e82d73ae24cceefd7f6f085058f
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 00:10:13 -0300

    THOR 7 V3.13 sincronizar cache

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.12 - restaura Análise e Combinações nos cards da tela inicial; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-12');
+// THOR 7 V3.13 - remove seta e botão Salvar Jogos do cabeçalho do Gerador de Palpites; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-13');
