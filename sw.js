2902e89864f6570d681d0594317889f16bdf91aa
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 05:56:50 -0300

    THOR 7 V3.25 centralizar e reduzir números das etapas

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.24 - remover somente o botão Gerar Novos; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-24');
+// THOR 7 V3.25 - números 1, 2 e 3 menores e centralizados; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-25');
