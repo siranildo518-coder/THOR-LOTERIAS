e69168d189e6e1d08621f475e5cb6749f1a5ac4a
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 21:42:57 -0300

    THOR 7 V3.4 sincronizar cache

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.3 - tabela integrada diretamente em Começo e Final; arquivos e cache sincronizados.
-importScripts('./sw-core-thor7.js?v=thor7-v3-3');
+// THOR 7 V3.4 - Posicionamento usa histórico oficial da Lotofácil; arquivos e cache sincronizados.
+importScripts('./sw-core-thor7.js?v=thor7-v3-4');
