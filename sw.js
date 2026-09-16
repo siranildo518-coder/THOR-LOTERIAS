b692fcf62feea5061f0e48163585041f5d24d404
siranildo518-coder <siranildo518@gmail.com>
2026-09-15 21:02:04 -0300

    THOR 7 V3.1: remover seta de Jogos Oficiais

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.0 - corrigir cliques do menu lateral; arquivos e cache sincronizados.
-importScripts('./sw-core-thor7.js?v=thor7-v3-0');
+// THOR 7 V3.1 - cabeçalho sem seta em Jogos Oficiais; arquivos e cache sincronizados.
+importScripts('./sw-core-thor7.js?v=thor7-v3-1');
