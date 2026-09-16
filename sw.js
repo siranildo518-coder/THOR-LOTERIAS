c021c9687639d469606f24db468729608af922cd
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 05:39:02 -0300

    THOR 7 V3.20 reduzir botões das loterias no Gerador

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.19 - foto no cabeçalho e botões menores no Gerador; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-19');
+// THOR 7 V3.20 - botões das loterias menores no Gerador; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-20');
