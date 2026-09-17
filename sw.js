ac07c234bdff32ed1808be4be21f5ba4980c74c5
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 15:24:34 -0300

    THOR 7 V95 atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V94 - design da tela Jogos Salvos; login e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v94');
+// THOR 7 V95 - Jogos Salvos mais fino e bolinhas menores; login e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v95');
