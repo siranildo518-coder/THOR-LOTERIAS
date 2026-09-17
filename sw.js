3d100dbd1a9541f35fcd2cd4b012d4c4ff6c3d3e
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 17:17:41 -0300

    THOR 7 V99 atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V98 - Salvar Jogos abre diretamente Jogos Salvos; login e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v98');
+// THOR 7 V99 - exibe os jogos gravados em Jogos Salvos; login e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v99');
