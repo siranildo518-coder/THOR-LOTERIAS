4afaddff76f6291194e60f8f218cc10250f45c07
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 20:42:06 -0300

    THOR 7 V116 - atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V115 - corrige COPIAR ao lado do campo Usar quantas dezenas; login, senha e trava preservados sem alteracao.
-importScripts('./sw-core-thor7.js?v=thor7-v115');
+// THOR 7 V116 - abertura direta na tela inicial confirmada; login, senha e trava preservados sem alteracao.
+importScripts('./sw-core-thor7.js?v=thor7-v116');
