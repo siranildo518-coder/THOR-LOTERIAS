4b74c27dc39949da12f1c4151d3518e4101feadc
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 18:42:55 -0300

    THOR 7 V105 atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V104 - resultados oficiais dos Jogos Salvos atualizados pela API da CAIXA; login e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v104');
+// THOR 7 V105 - Posicionamento usa quantidade exata digitada e remove faixa das loterias; login e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v105');
