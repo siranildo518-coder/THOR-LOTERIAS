8ea0b73e570385308342c7550f9ea7a9dfa05fea
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 22:52:55 -0300

    Publicar correção do primeiro acesso V3.55

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.54 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-54');
+// THOR 7 V3.55 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-55');
