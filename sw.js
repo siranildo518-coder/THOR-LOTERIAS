e0d4160a15149f4dca277325f5d997f3e17eea34
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 03:37:11 -0300

    Restaurar THOR 7 V3.62

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.63 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-63');
+// THOR 7 V3.62 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-62');
