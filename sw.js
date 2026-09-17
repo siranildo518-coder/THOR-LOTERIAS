83182c7544694ed5be96ba0af538f5e9a3466353
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 06:09:24 -0300

    V3.87 sincroniza cache do posicionamento

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.86 - força atualização do cache/carregador sem alterar login ou trava.
-importScripts('./sw-core-thor7.js?v=thor7-v3-86');
+// THOR 7 V3.87 - corrige carregamento do posicionamento sem alterar login ou trava.
+importScripts('./sw-core-thor7.js?v=thor7-v3-87');
