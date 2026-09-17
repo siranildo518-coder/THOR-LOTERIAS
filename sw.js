8651b745caa8bc509cb0b8d9526c9b9eb063970d
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 23:05:15 -0300

    Publicar trava Firestore V3.58

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.57 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-57');
+// THOR 7 V3.58 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-58');
