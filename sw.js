1b36b91e382eb7d49c3fc57661833d6475530ab8
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 19:48:51 -0300

    THOR 7 V113 - atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V112 - botao COLAR entre GERAR e LIMPAR; login e trava preservados sem alteracao.
-importScripts('./sw-core-thor7.js?v=thor7-v112');
+// THOR 7 V113 - somente Dupla Sena mostra 1o sorteio em cima e 2o embaixo; demais loterias preservadas; login e trava intocados.
+importScripts('./sw-core-thor7.js?v=thor7-v113');
