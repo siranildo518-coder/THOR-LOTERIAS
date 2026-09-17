8b31e5fba3dab4483205c3018bc1bb2e2549cdc4
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 04:10:23 -0300

    Ativar service worker THOR 7 V3.70

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.69 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-69');
+// THOR 7 V3.70 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-70');
