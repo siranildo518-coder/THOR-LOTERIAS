1bc25785e385d335a5e50bb11ee406e00f9d46f2
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 03:44:36 -0300

    Atualizar cache para THOR 7 V3.64

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.62 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-62');
+// THOR 7 V3.64 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-64');
