bc2a5f05c71e6f2f1c17dc117bd49b7d712e16bf
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 22:56:09 -0300

    Publicar bloqueio completo do navegador V3.57

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.56 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-56');
+// THOR 7 V3.57 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-57');
