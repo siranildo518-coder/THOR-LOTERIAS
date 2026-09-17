459905120523ea9d6e0de09c0186863f59da7f8e
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 18:01:44 -0300

    THOR 7 V102 atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V101 - remove faixa completa de botões das loterias em Posicionamento; login e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v101');
+// THOR 7 V102 - posicionamento atualiza automaticamente ao digitar; login e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v102');
