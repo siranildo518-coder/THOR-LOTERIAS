b7184f0da9a61fc1408855988fe732bac3b95f79
siranildo518-coder <siranildo518@gmail.com>
2026-09-16 22:55:42 -0300

    Publicar trava somente aplicativo V3.56

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V3.55 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
-importScripts('./sw-core-thor7.js?v=thor7-v3-55');
+// THOR 7 V3.56 - topo da Mega-Sena e bolinhas coloridas no resultado; cache sincronizado.
+importScripts('./sw-core-thor7.js?v=thor7-v3-56');
