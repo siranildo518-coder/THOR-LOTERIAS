13c5a501764e5cdd69c66e1c6d2dbd3748e91703
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 11:20:40 -0300

    THOR 7 V93 atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V92 - botão Jogos Salvos na tela inicial; login e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v92');
+// THOR 7 V93 - Jogos Salvos independente; login e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v93');
