ebf48f918d06ede996402fc4ff7cd6e5f18c44a8
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 16:17:14 -0300

    THOR 7 V97 atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V96 - botão compacto Salvar Jogos; login e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v96');
+// THOR 7 V97 - corrige botão Salvar Jogos no local exato; login e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v97');
