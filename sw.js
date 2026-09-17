4c833aa2eb4941e3502f2f928fa950e289c2b15b
siranildo518-coder <siranildo518@gmail.com>
2026-09-17 17:59:27 -0300

    THOR 7 V101 atualiza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V100 - nomes e filtros dos Jogos Salvos; login e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v100');
+// THOR 7 V101 - remove faixa completa de botões das loterias em Posicionamento; login e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v101');
