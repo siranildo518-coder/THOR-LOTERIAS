687cb2e129c74fb10927ae3a5e461e649e250ef3
siranildo518-coder <siranildo518@gmail.com>
2026-09-20 05:36:44 -0300

    Ativa service worker oficial v125

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR 7 V117 TESTE - atualizacao em segundo plano sem recarregar a tela atual; login, senha e trava preservados sem alteracao.
-importScripts('./sw-core-thor7.js?v=thor7-v119-falhas-concursos');
+// THOR LOTERIAS V125 - cache sincronizado; login, senha e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v125-estavel-1');
