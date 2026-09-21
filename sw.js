218ca87546a9790e5c8530c0df67d206272a6299
siranildo518-coder <siranildo518@gmail.com>
2026-09-21 19:29:27 -0300

    Atualizar service worker V142 cadastro imediato

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V142 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v142-zero-flash');
+importScripts('./sw-core-thor7.js?v=thor7-v142-cadastro-imediato');
