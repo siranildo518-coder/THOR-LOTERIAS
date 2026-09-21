5fe1d90c8f42ccffeeb16e2d7b7989538a66a631
siranildo518-coder <siranildo518@gmail.com>
2026-09-21 17:12:14 -0300

    Atualizar service worker V139

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR LOTERIAS V138 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v138-login-fix');
+// THOR LOTERIAS V139 - cache sincronizado; login, senha e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v139-gisele-sem-trava');
