cefb04b70cc410c3e883ad85deae9187c7a92a79
siranildo518-coder <siranildo518@gmail.com>
2026-09-21 06:48:09 -0300

    Atualizar service worker para Falhas sequencial

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V125 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v125-estavel-1');
+importScripts('./sw-core-thor7.js?v=thor7-v125-estavel-2-sequencial');
