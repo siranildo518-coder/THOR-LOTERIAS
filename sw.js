d9fe661de8e12defbcb05c941363ec3e3cfe7145
siranildo518-coder <siranildo518@gmail.com>
2026-09-21 19:31:49 -0300

    Atualizar service worker V142 cadastro fix2

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR LOTERIAS V142 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v142-cadastro-imediato');
+// THOR LOTERIAS V142- cache sincronizado; login, senha e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v142-cadastro-fix2');
