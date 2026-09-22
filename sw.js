96a495b795d93ab6e25bc9ca7ca80e117d3671c8
siranildo518-coder <siranildo518@gmail.com>
2026-09-21 23:12:19 -0300

    THOR 7.44: sincroniza Jogos salvos sem seta

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V144- cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v144-fundo-branco-sem-salvar');
+importScripts('./sw-core-thor7.js?v=thor7-v144-jogos-salvos-sem-seta');
