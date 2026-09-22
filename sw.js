f5bfda63ac2a12b8283bf2cc9a3eaf02f09dc60f
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 05:46:54 -0300

    THOR 7 V145: sincroniza jogos compactos

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V145- cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v145-jogos-salvos-filtros');
+importScripts('./sw-core-thor7.js?v=thor7-v145-jogos-compactos-jogos-compactos');
