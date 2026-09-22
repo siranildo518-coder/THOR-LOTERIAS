bb5d238f7a39c80545db10406bf71af4173655e4
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 06:48:20 -0300

    Restaurar sw.js para THOR 7 V146

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR LOTERIAS V150 - botões restaurados; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v150-botoes-restaurados');
+// THOR LOTERIAS V146- cache sincronizado; login, senha e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos');
