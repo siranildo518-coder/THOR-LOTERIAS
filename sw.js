a0359a49011c459c40c4ab106ce2ab995f39c50a
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 06:02:24 -0300

    THOR 7 V145: sincroniza filtros conforme palpites

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V145- cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v145-filtros-mais-baixos-filtros-mais-baixos-jogos-compactos');
+importScripts('./sw-core-thor7.js?v=thor7-v145-filtros-conforme-palpites-filtros-mais-baixos-filtros-mais-baixos-jogos-compactos');
