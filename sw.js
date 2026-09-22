c7828755699814f74151bbb312269396c422039e
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 17:59:03 -0300

    V146: sincronizar bloqueio UB487LPD

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 // THOR LOTERIAS V146- cache sincronizado; login, senha e trava preservados.
-// Roberto Cruz: dois dispositivos permitidos, senha preservada
-importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos&fix=roberto-2-devices');
+// código UB487LPD bloqueado
+importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos&fix=bloqueio-ub487lpd');
