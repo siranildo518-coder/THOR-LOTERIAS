f2496b5a863e9b38a8cd7065c004ba0efb6f6b03
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 05:38:13 -0300

    THOR 7 V145: sincroniza jogos salvos com filtros

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V145- cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v145');
+importScripts('./sw-core-thor7.js?v=thor7-v145-jogos-salvos-filtros');
