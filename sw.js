2601d6af845c835d73852745e1709f212c34736f
siranildo518-coder <siranildo518@gmail.com>
2026-09-21 22:15:48 -0300

    THOR 7.44: sincroniza Salvar jogos

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V144- cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-V144-cadastro-login-fix');
+importScripts('./sw-core-thor7.js?v=thor7-v144-salvar-jogos-nome');
