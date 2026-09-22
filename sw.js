cfcd0278b7e61145a61a062712ec60f4b77ffc68
siranildo518-coder <siranildo518@gmail.com>
2026-09-21 21:27:13 -0300

    Restaura THOR 7.44: sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR LOTERIAS V146- cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-V146-cadastro-login-fix');
+// THOR LOTERIAS V144- cache sincronizado; login, senha e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-V144-cadastro-login-fix');
