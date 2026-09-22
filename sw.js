8f53982d3542631f598784ae9c2430f1c8703cad
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 06:36:05 -0300

    THOR 7 V147: sincroniza service worker

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-// THOR LOTERIAS V146- cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos');
+// THOR LOTERIAS V147 - index direto; login, senha e trava preservados.
+importScripts('./sw-core-thor7.js?v=thor7-v147-index-direto');
