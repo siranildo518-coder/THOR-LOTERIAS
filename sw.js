3cb99c27002863f2e2a4eeadf9bbd6ef8e0a2997
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 18:00:43 -0300

    V146: sincronizar bloqueio do usuário Adelino

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 // THOR LOTERIAS V146- cache sincronizado; login, senha e trava preservados.
-// código UB487LPD bloqueado
-importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos&fix=bloqueio-ub487lpd');
+// usuário Adelino bloqueado
+importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos&fix=bloqueio-adelino');
