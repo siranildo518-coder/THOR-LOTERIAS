7dc4569032b1e728de265c6df7447b56f7c254a0
siranildo518-coder <siranildo518@gmail.com>
2026-09-22 07:41:36 -0300

    V146: sincronizar abertura sem roxo

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
 // THOR LOTERIAS V146- cache sincronizado; login, senha e trava preservados.
-// ajuste de abertura: tela inicial única, sem troca tardia do cabeçalho
-importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos&fix=inicio-unico');
+// ajuste de abertura: remove tela roxa inicial, mantendo V146
+importScripts('./sw-core-thor7.js?v=thor7-v146-abertura-direta-filtros-compactos&fix=sem-roxo');
