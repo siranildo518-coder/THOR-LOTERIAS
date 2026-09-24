dcfe0846af97448b0546e1bbbc81dcf7a924e3a8
siranildo518-coder <siranildo518@gmail.com>
2026-09-24 08:12:31 -0300

    Atualizar cache da correção de cadastro

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V144 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v150-login-marcus');
+importScripts('./sw-core-thor7.js?v=thor7-v150-cadastro-login-fix');
