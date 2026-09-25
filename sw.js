5a2fa1c3b23a3f77f55b513b064ecde516b3adfc
siranildo518-coder <siranildo518@gmail.com>
2026-09-25 05:57:36 -0300

    Sincroniza cache da rolagem Jogos Desdobrados

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V144 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-v150-sorteio-fix2');
+importScripts('./sw-core-thor7.js?v=thor7-v150-scroll-resultados-fix1');
