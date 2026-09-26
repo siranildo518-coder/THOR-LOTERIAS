8c3ef354ca1362a3d68f27022693a3d4072dd191
siranildo518-coder <siranildo518@gmail.com>
2026-09-26 14:54:55 -0300

    THOR 7 OFICIAL C5: ativa atualização automática

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
 // THOR LOTERIAS V144 - cache sincronizado; login, senha e trava preservados.
-importScripts('./sw-core-thor7.js?v=thor7-oficial-c4');
+importScripts('./sw-core-thor7.js?v=thor7-oficial-c5');
